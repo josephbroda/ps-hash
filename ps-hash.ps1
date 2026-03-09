79607db3a75df0a8cb8bd22591ebcfa47bbd1fe9
Joseph Broda <josephbroda@icloud.com>
2026-03-09 14:16:11 -0700

    Cleaned up logic and syntax.

--- ps-hash.ps1
+++ ps-hash.ps1
@@ -4,32 +4,26 @@
 
 #Get user input for file and official hash
 $filePath = Read-Host "Enter the full path to the file"
-$expectedHash = Read-Host "Paste the expected hash here"
-$algorithm = Read-Host "What algorithm is the hash? `n1. MD5`n2. SHA1 `n3. SHA256`n"
+$expectedHash = (Read-Host "Paste the expected hash here").Trim()
+$choice = Read-Host "What algorithm is the hash? `n1. MD5`n2. SHA1 `n3. SHA256`n"
 
 #Select hash algorithm
-if ($algorithm -eq 1) {
-    $selectedAlgorithm = "MD5"
+$selectedAlgorithm = switch ($choice) {
+    1 { "MD5" }
+    2 { "SHA1" }
+    3 { "SHA256" }
+    Default { Write-Host "Invalid choice"; exit }
 }
-elseif ($algorithm -eq 2) {
-    $selectedAlgorithm = "SHA1"
-}
-elseif ($algorithm -eq 3) {
-    $selectedAlgorithm = "SHA256"
-}
-else {
-    Write-Host "Incorrect option. Try again."
-    exit
-} 
 
 #Generate hash for local file
-$actualHash = (get-FileHash -Path $filePath -Algorithm $selectedAlgorithm).Hash
+$actualHash = (Get-FileHash -LiteralPath $filePath -Algorithm $selectedAlgorithm).Hash
 Write-Host "Calculated hash using "$selectedAlgorithm": " $actualHash
+Write-Host "Expected hash: " $expectedHash
 
 #Perform comparison
 if ($actualHash -eq $expectedHash) {
-    Write-Host "The hashes match. This file is safe." -ForegroundColor Green
+    Write-Host "SUCCESS: Hashes match!" -ForegroundColor Green
 }
 else {
-    Write-Host "The hashes do NOT match. The file may be corrupted or tampered with." -ForegroundColor Red
+    Write-Host "WARNING: Hashes do NOT match!" -ForegroundColor Red
 }
